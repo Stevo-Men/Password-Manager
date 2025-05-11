8c437233856b7fa7fd87b14a4e85700d10a5ea34
Stevo.Men <stevemenard3@gmail.com>
2025-05-10 23:24:45 -0400

    feat: encryption added

---
+++ app/Models/Inventory/Services/RegisterService.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Models\Inventory\Services;
+
+use Models\Inventory\Entities\User;
+use Models\Inventory\Services\Encyryption\EncyrptionService;
+use Models\Inventory\Validators\RegisterValidator;
+use Models\Inventory\Brokers\UserBroker;
+use Zephyrus\Application\Form;
+use Zephyrus\Security\Cryptography;
+use Zephyrus\Network\Router\Post;
+
+class RegisterService
+{
+    private UserBroker $broker;
+    private EncyrptionService $encrypt;
+    private RegisterValidator $validator;
+
+    public function __construct()
+    {
+        $this->broker = new UserBroker();
+        $this->encrypt = new EncyrptionService();
+        $this->validator = new RegisterValidator();
+    }
+
+    #[Post("/register")]
+    public function register($form): array
+    {
+        try {
+            $this->validator->assert($form, $this->broker);
+
+            if ($form->getErrors()) {
+                return [
+                    'form' => $form,
+                    'errors' => $form->getErrorMessages()
+                ];
+            }
+
+            $firstname = $form->getValue('firstname');
+            $lastname = $form->getValue('lastname');
+            $password = $form->getValue('password');
+            $email = $form->getValue('email');
+            $username = $form->getValue('username');
+
+            $salt = $this->encrypt->generateSalt();
+            $userKey = $this->encrypt->createUserKey($password, $salt);
+            $hash = Cryptography::hashPassword($password);
+
+            $user = new User();
+            $user->firstName = $this->encrypt->encrypt($firstname, $userKey);
+            $user->lastName = $this->encrypt->encrypt($lastname, $userKey);
+            $user->username = $this->encrypt->encrypt($username, $userKey);
+            $user->email = $this->encrypt->encrypt($email, $userKey);
+            $user->password_hash = $hash;
+            $user->salt = $salt;
+            $user->email_hash = $this->encrypt->simpleHash($email);
+            $newUserId = $this->broker->insert($user);
+
+
+            EncyrptionService::setUserContext($newUserId, $userKey);
+
+            return [
+                'form' => $form
+            ];
+        } catch (\Exception) {
+            return [
+                'form' => $form,
+                'errors' => $form->getErrorMessages()
+            ];
+        }
+    }
+}
---
+++ app/Models/Inventory/Validators/RegisterValidator.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Models\Inventory\Validators;
+
+use Models\Exceptions\FormException;
+use Models\Inventory\Services\Encyryption\EncyrptionService;
+use Zephyrus\Application\Form;
+use Zephyrus\Application\Rule;
+
+class RegisterValidator
+{
+    public static function assert(Form $form, $broker): void
+    {
+        $form->field("firstname", [
+            Rule::required("Le prénom est obligatoire")
+        ]);
+        $form->field("lastname", [
+            Rule::required("Le nom est obligatoire")
+        ]);
+
+        $form->field("username", [
+            Rule::required("Le username est obligatoire")
+        ]);
+        $form->field("username", [
+            Rule::required("Le username est obligatoire")
+        ]);
+        $form->field("password", [
+            Rule::required("Le password est obligatoire"),
+            Rule::minLength(8,"Longueur minimale de 8 caractères")
+        ]);
+        $form->field("password_confirm", [
+            Rule::required("Le password est obligatoire"),
+            Rule::minLength(8,"Longueur minimale de 8 caractères")
+        ]);
+        $form->field("email", [
+            Rule::required("Le password est obligatoire"),
+            Rule::email("Longueur minimale de 8 caractères")
+        ]);
+
+        $password = $form->getValue("password");
+        $password_confirm = $form->getValue("password_confirm");
+        $email = $form->getValue("email");
+        $emailHash = (new EncyrptionService)->simpleHash($email);
+        $username = $form->getValue("username");
+
+        if ($password !== $password_confirm) {
+            $form->addError('password_confirm', "Les deux mots de passe ne correspondent pas");
+        }
+        if ($broker->findByUsername($username)) {
+            $form->addError('username', "Ce nom d'utilisateur est déjà pris");
+        }
+        if ($broker->findByEmailHash($emailHash)) {
+            $form->addError('email', "Cette adresse email est déjà utilisée");
+        }
+
+        if (!$form->verify()) {
+            throw new FormException($form);
+        }
+    }
+}
